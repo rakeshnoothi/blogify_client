84096c33eb2b91fb3769663414af83ed63172a98
Rakesh <rakeshnoothi9@gmail.com>
2023-09-05 19:07:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Box, Container } from "@mui/material";
+
+const App = () => {
+    return (
+        <Box>
+            <Container maxWidth="lg"></Container>
+        </Box>
+    );
+};
+
+export default App;
